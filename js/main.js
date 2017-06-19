3d3c32a2c49c0a8cbe91f8700f72397bc9fac70a
Toby T Castro <tc6586@gmail.com>
2017-06-19 00:49:33 -0500

    fixed all masonry grid settings and input first post box content with proper spacings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 //Just starting the main.js file
+
+$('.post-grid').masonry({
+  itemSelector: '.post-item',
+  columnWidth: '.post-sizer',
+  isFitWidth: true, 
+  gutter: 30,
+  horizontalOrder: true
+});
